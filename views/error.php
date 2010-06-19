6e3e0eac84cecf8a570d7e25270ee8d21ceca716
Anthony Short <anthonyshort@me.com>
2010-06-19 13:15:59 +1000

    Updated the styling of the error view

--- views/error.php
+++ views/error.php
@@ -6,11 +6,11 @@
 	
 	<style>
 		html { background:#e7e7e7; }
-		h1 { font-size:18px; margin:9px 0 9px 0;}
+		h1 { font-size:24px; margin:9px 0 18px 0;}
 		p {margin:0 0 18px 0;}
-		h2 { font-size:16px; }
+		h2 { font-size:18px; }
 		strong {color:#000}
-		pre {background:#eee;padding:20px;font-size:13px;}
+		pre {background:#eee;padding:20px;font-size:13px;-webkit-border-radius:5px}
 		
 		.content 
 		{ 
@@ -18,13 +18,13 @@
 			max-width:60%; 
 			margin:30px auto; 
 			font:15px/18px Arial; 
-			padding:18px 20px; 
+			padding:18px 20px 0; 
 			background:#fff;
 			color:#595959; 
 			border:1px solid #aaa; 
 			margin-bottom: 20px; 
-			-webkit-border-radius:4px; 
-			-moz-border-radius:4px; 
+			-webkit-border-radius:6px; 
+			-moz-border-radius:6px; 
 			-webkit-box-shadow:0 2px 5px rgba(0,0,0,0.2);
 		}
 		
@@ -40,25 +40,40 @@
 			color:#ffe5e5;
 			font-size:12px;
 			text-shadow:0 -1px 0 rgba(0,0,0,0.4);
-			margin:0 -21px -19px;
+			margin:21px -21px 0;
 			padding:0 20px 0;
-			line-height:30px;
+			line-height:30px;			
+			-webkit-animation-iteration-count:infinite;
+			-webkit-animation-duration:5s;
+			-webkit-animation-name:unicorn;
+			-webkit-animation-timing-function:linear;
+		}
+		
+		@-webkit-keyframes unicorn 
+		{
+			from	{ background-position:0 0 }
+			to		{ background-position:222px 0 }
 		}
 		
 		.meta.bottom
 		{
-			-webkit-border-bottom-left-radius:4px; -moz-border-bottomleft-radius:4px; 
-			-webkit-border-bottom-right-radius:4px; -moz-border-bottomright-radius:4px; 
+			-webkit-border-bottom-left-radius:6px; -moz-border-bottomleft-radius:6px; 
+			-webkit-border-bottom-right-radius:6px; -moz-border-bottomright-radius:6px; 
 		}
 		
 		.meta p {margin:0}
+		
+		code{
+			color:#c24a4a;
+			font-size:14px;
+		}
 
 	</style>
 </head>
 <body>
 	
 	<div class="content">
-		<h1><?php echo $code; ?></h1>
+		<h1><?php echo $title; ?></h1>
 		<p id="message"><?php echo $message; ?></p>
 		<div class="meta bottom">
 			<p><?php echo "$file [$line]"; ?></p>
