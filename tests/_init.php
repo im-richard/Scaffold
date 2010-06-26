bda812cb226e70a0718a0ffdb170bdf65119b8a5
Anthony Short <anthonyshort@me.com>
2010-06-26 21:16:57 +1000

    Adjusted the path

--- tests/_init.php
+++ tests/_init.php
@@ -1,5 +1,5 @@
 <?php
 
 require_once 'PHPUnit/Framework.php';
-require_once __DIR__ .'/../Core/lib/Environment.php';
+require_once __DIR__ .'/../lib/Environment.php';
 spl_autoload_register(array('Environment','auto_load'));
