1e8783614b2cdf77dd4bfb5d70d3fdfd821f312c
Anthony Short <anthonyshort@me.com>
2010-07-10 15:59:44 +1000

    * Added new Properties extension. This lets extensions define custom properties. Used in the same way as the Functions extension. * Added the image-replace extension. This creates a new property called image-replace that fetches the height and width of the image and produces the properties needed to image replace an object.

---
+++ extensions/Properties/Properties.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Scaffold_Extension_Properties
+ *
+ * Allows other extensions to register custom properties.
+ * 
+ * @package 		Scaffold
+ * @author 			Anthony Short <anthonyshort@me.com>
+ * @copyright 		2009-2010 Anthony Short. All rights reserved.
+ * @license 		http://opensource.org/licenses/bsd-license.php  New BSD License
+ * @link 			https://github.com/anthonyshort/csscaffold/master
+ */
+class Scaffold_Extension_Properties extends Scaffold_Extension
+{	
+	/**
+	 * @var array
+	 */
+	public $properties = array();
+	
+	/**
+	 * Lets extensions register custom functions by creating a hook
+	 * @access public
+	 * @param $source Scaffold_Source
+	 * @return void
+	 */
+	public function initialize($source)
+	{
+		$this->scaffold->notify('register_property',array($this));
+	}
+	
+	/**
+	 * Register a new function
+	 * @access public
+	 * @param $name
+	 * @param $map
+	 * @return void
+	 */
+	public function register($name,$map)
+	{
+		$this->properties[$name] = $map;
+	}
+	
+	/**
+	 * @access public
+	 * @param $source
+	 * @return string
+	 */
+	public function post_process($source)
+	{
+		// Go through each custom function
+		foreach($this->properties as $name => $property)
+		{
+			// Find them in the CSS
+			foreach(Scaffold_Helper_CSS::find_properties($name,$source->contents) as $found)
+			{
+				$obj 	= $property[0];
+				$method = $property[1];
+				
+				// Call the hook method for this function
+				$result = call_user_func_array(array($obj,$method),array($found['value']));
+				
+				// Replace it in the CSS
+				$source->contents = str_replace($found['property'],$result,$source->contents);
+			}
+		}
+	}
+}
