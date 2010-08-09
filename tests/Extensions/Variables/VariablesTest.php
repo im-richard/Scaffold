c4e7f5d6800d26172cedf8eb3a06a5131f4e7f33
Anthony Short <anthonyshort@me.com>
2010-08-09 22:13:05 +1000

    * Added Variables unit test

---
+++ tests/Extensions/Variables/VariablesTest.php
@@ -0,0 +1,39 @@
+<?php
+
+class Scaffold_Extension_VariablessTest extends PHPUnit_Framework_TestCase
+{
+	private $source;
+	private $scaffold;
+	private $object;
+	
+	protected function setUp()
+	{
+		$system 			= realpath(__DIR__.'/../../../');
+		$container 			= new Scaffold_Container($system);
+		$this->scaffold 	= $container->build();
+		$this->object 		= new Scaffold_Extension_Variables();
+		
+		$_SERVER['DOCUMENT_ROOT'] = $system;
+	}
+	
+	/**
+	 * @test
+	 */
+	public function test_process()
+	{
+		$dir = __DIR__ . '/_files/original/';
+		
+		foreach(glob($dir.'*.css') as $original)
+		{
+			# The expected output
+			$expected = file_get_contents(str_replace('/_files/original/','/_files/expected/',$original));
+			
+			# Create and parse the source
+			$source = new Scaffold_Source_File($original);
+			$this->object->process($source,$this->scaffold);
+			
+			# The source contents should equal the expect output
+			$this->assertEquals($expected,$source->contents);
+		}
+	}
+}
