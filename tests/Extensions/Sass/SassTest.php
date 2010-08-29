e5c6f3a761cad06fc841e5a9d06712d9570eb261
Anthony Short <anthonyshort@me.com>
2010-08-29 13:33:13 +1000

    Updated Sass unit tests

--- tests/Extensions/Sass/SassTest.php
+++ tests/Extensions/Sass/SassTest.php
@@ -17,7 +17,28 @@ class Scaffold_Extension_SassTest extends PHPUnit_Framework_TestCase
 	/**
 	 * @test
 	 */
-	public function test_process()
+	public function test_scss()
+	{
+		$dir = __DIR__ . '/_files/original/';
+		
+		foreach(glob($dir.'*.css') as $original)
+		{
+			# The expected output
+			$expected = file_get_contents(str_replace('/_files/original/','/_files/expected/',$original));
+			
+			# Create and parse the source
+			$source = new Scaffold_Source_File($original);
+			$this->object->process($source,$this->scaffold);
+
+			# The source contents should equal the expect output
+			$this->assertEquals($expected,$source->contents);
+		}
+	}
+	
+	/**
+	 * @test
+	 */
+	public function test_sass()
 	{
 		$dir = __DIR__ . '/_files/original/';
 		
