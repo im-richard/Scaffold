f0bdc5d7b60a591beb87e5e346a06c2e562b61e5
Anthony Short <anthonyshort@me.com>
2010-09-07 21:23:12 +1000

    Updated the time extension. It now creates variables based on the current time.

--- extensions/Time/Time.php
+++ extensions/Time/Time.php
@@ -1,43 +1,89 @@
 <?php
 
 /**
- * Time
+ * Scaffold_Extension_Time
  *
- * Sets different flags for different times and dates.
- * 
- * @author Anthony Short
+ * Adds various time-based functionality to Scaffold.
+ * - Adds time variables if the Variables extension is enabled
+ *
+ * @package 		Scaffold
+ * @author 			Anthony Short <anthonyshort@me.com>
+ * @copyright 		2009-2010 Anthony Short. All rights reserved.
+ * @license 		http://opensource.org/licenses/bsd-license.php  New BSD License
+ * @link 			https://github.com/anthonyshort/csscaffold/master
+ * @link			http://oocss.org/spec/css-variables.html
  */
-class Time extends Scaffold_Extension
+class Scaffold_Extension_Time extends Scaffold_Extension
 {
-	protected static $types = array
+	protected $types = array
 	(
-		'hour' => 'G',
-		'date' => 'j',
-		'day' => 'l',
-		'day_of_year' => 'z',
-		'week' => 'W',
-		'month' => 'F',
-		'year' => 'Y'
+		'hour' 			=> 'G',
+		'date' 			=> 'j',
+		'day' 			=> 'l',
+		'day_of_year' 	=> 'z',
+		'week' 			=> 'W',
+		'month'			=> 'F',
+		'year' 			=> 'Y'
+	);
+	
+	/**
+	 * Default settings which are used if the configuration
+	 * settings from the file aren't set.
+	 * @var array
+	 */
+	public $_defaults = array(
+		'offset' => 0
 	);
 	
 	/**
 	 * The current times
 	 */
-	public static $current = array();
+	public $current = array();
+	
+	public function __construct($config = array())
+	{
+		// Run the standard constructors
+		parent::__construct($config);
+		
+		// Set the current time in the local array
+		$now = time() + (60 * 60 * $this->config['offset']);
+		$this->set_current_time($now);
+	}
+	
+	/**
+	 * Create variables for the current time
+	 * @access public
+	 * @param $source
+	 * @param $scaffold
+	 * @return string
+	 */
+	public function variables_replace($source,$variables)
+	{
+		$time = array();
+		
+		// Add each of the basic units
+		foreach(array_keys($this->types) as $unit)
+		{
+			$time[$unit] = $this->current[$unit];
+		}
+		
+		// Last modified time of the file
+		$time['now'] = gmdate('D, d M Y H:i:s', time());
+		
+		// Create the var array
+		$vars = array('time'=>$time);
+		
+		// And merge it 
+		$variables->variables = array_merge($variables->variables,$vars);
+	}
 	
 	/**
 	 * Sets flags for different times of the day
-	 *
-	 * @author Anthony Short
 	 * @return void
 	 */
-	public static function flag()
+	public function flag()
 	{
-		$now = time() + (60 * 60 * Scaffold::$config['Time']['offset']);
-		
-		self::set_current_time($now);
-
-		$condition_types = array_keys(self::$types);
+		$condition_types = array_keys($this->types);
 		
 		foreach(Scaffold::$config['Time']['flags'] as $flag_name => $conditions)
 		{
@@ -73,34 +119,26 @@ class Time extends Scaffold_Extension
 	 * @author Anthony Short
 	 * @return return type
 	 */
-	private static function set_current_time($now)
+	private function set_current_time($now)
 	{		
-		foreach(self::$types as $type => $id)
+		foreach($this->types as $type => $id)
 		{
-			self::$current[$type] = date($id,$now);		
+			$this->current[$type] = date($id,$now);		
 		}
 	}
 	
 	/**
 	 * Checks to see if it's between the two numbers
-	 *
-	 * @author Anthony Short
 	 * @param $from
 	 * @param $to
+	 * @param $type
+	 * @param $now
 	 * @return boolean
 	 */
-	private static function is_between($from,$to,$type,$now)
+	private function is_between($from,$to,$type,$now)
 	{
-		$check = date(self::$types[$type],$now);
-		
-		if($check >= $from && $check <= $to)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		$check = date($this->types[$type],$now);
+		return ($check >= $from && $check <= $to);
 	}	
 	
 }
